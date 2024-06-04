741f258addae237f53007449bc433a96c1fbf418
wweavishayaknin <WWEAVISHAYAKNIN@GMAIL.COM>
2024-06-05 00:03:55 +0300

    Update pikudhaorefalerts.py

--- pikudhaorefalerts.py
+++ pikudhaorefalerts.py
@@ -81,7 +81,7 @@ def merge_dataframes(df_alertoref, mapping_df, df_weather):
         result_df = pd.merge(df_alertoref, mapping_df, how='left', left_on='data', right_on='hebrewcity')
 
         if isinstance(df_weather, pd.DataFrame):
-            merged_df = pd.merge(result_df, df_weather, how='inner', left_on='englishcity', right_on='Station_Name')
+            merged_df = pd.merge(result_df, df_weather, how='left', left_on='englishcity', right_on='Station_Name')
             return merged_df
         else:
             return None
